6727f2490f3fada33d4c2c25fb7bf36f34f6c30a
aem2231 <mariojetullaji@gmail.com>
2025-01-21 14:46:20 +0000

    testing selenium

--- web.py
+++ web.py
@@ -1,3 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import time
+
+def access_website():
+    driver = webdriver.Firefox() # Initialize a Firefox webdriver
+    driver.get("https://launch.huggg.me/super/p7iPuMm9h4lRiM9")
+    html = browser.page_source
+    print(html)
+    browser.close()
+
+access_website()
