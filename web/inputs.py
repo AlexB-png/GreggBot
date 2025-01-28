5b7ed8f0e204939423ae926a985dc1edd53dfb13
UnoriginalUser15 <mrwillowmoody@gmail.com>
2025-01-28 12:44:25 +0000

    web stuff fully functional

--- web/inputs.py
+++ web/inputs.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 # a decorator used to tell the application
 # which URL is associated function
 @app.route('/')
-def website():
+def index():
     return render_template("index.html")
 
 @app.route('/email_input', methods=["POST"])
@@ -14,6 +14,13 @@ def get_email_input():
         # getting input with email = get_email)input in HTML form
         email = request.form.get("email")
         return email
+    else:
+        print("idk what has gone wrong but something has")
+
+@app.route('/new_link')
+def get_new_link():
+    if request.method == "GET":
+        return render_template("new_link.html")
     else:
         print("idk what has gone wrong but something has")
 
